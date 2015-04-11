0e72f8caf4f22a54e1ccb706b813f9ecb5c79d31
loki2302 <loki2302@loki2302.me>
2015-04-11 13:00:43 +0300

    more cleanup

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,8 @@ echo `date` > 1.txt
 git init
 git add .
 git commit -m "autocommit"
-git push -u https://github.com/loki2302/git-force-push-experiment.git master:deployed --force
+git push -u https://github.com/loki2302/git-force-push-experiment.git \
+  master:deployed --force
 cd ..
 rm -rf ./deploy
 echo DONE
